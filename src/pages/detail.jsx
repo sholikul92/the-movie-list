7a60c0a05b1570fc7658141c338c5e44b2f3822d
Sholikul Ardian <sholikulardian@gmail.com>
2024-01-30 21:55:53 +0700

    "fix: padding page detail"

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -21,7 +21,7 @@ const DetailPage = () => {
     <>
       <Header />
       <div className="h-screen flex flex-col-reverse lg:flex-row justify-end lg:justify-evenly items-center gap-5 bg-gradient-to-b from-slate-500 via-slate-800 to-slate-950">
-        <div className="text-white flex flex-col flex-wrap lg:w-[30em] gap-3 lg:gap-5 p-2">
+        <div className="text-white flex flex-col flex-wrap lg:w-[30em] gap-3 lg:gap-5 p-5">
           <h3 className="text-4xl lg:text-6xl font-semi-bold -ml-1">
             {movies.title}
           </h3>
